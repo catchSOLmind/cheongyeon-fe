ee26e5ecfb99e8da3b4f5db83ec1b95957cc9742
Simmee02 <20211002@sungshin.ac.kr>
2026-02-02 20:41:05 +0900

    feat : 피드백 완료 페이지

--- src/features/todo/pages/feedbackFinPage.tsx
+++ src/features/todo/pages/feedbackFinPage.tsx
@@ -5,16 +5,26 @@ import { BottomCTAButton } from '@/shared/components/BottomCTAButton';
 
 function FeedbackFinishPage() {
   return (
-    <div>
-      <Header title="피드백 남기기" showBackButton />
-      <img src={ImgFinish} alt="FeedbackFinishPage" />
-      <h1>FeedbackFinishPage</h1>
-      <BottomCTAWrapper>
-        <BottomCTAButton label="피드백 남기기">
-        </BottomCTAButton>
-      </BottomCTAWrapper>
+    <div className="min-h-screen">
+    <Header title="피드백 남기기" showBackButton />
+    <img
+        src={ImgFinish}
+        alt="FeedbackFinishPage"
+        className="mx-auto mt-14 w-[152px] h-[162px] object-cover"
+    />
+    <div className="mt-14 text-center text-display-xs text-black">
+    피드백 제출을 완료했어요
     </div>
-  );
+    <div className="mt-2 text-center text-body-m text-gray-800">
+    모아진 피드백은 우리집 리포트에서 확인할 수 있어요!
+    </div>
+    <BottomCTAWrapper className='mt-[227px]'>
+        <BottomCTAButton label="우리집으로 돌아가기" />
+    </BottomCTAWrapper>
+    </div>
+
+);
 }
+
 
 export default FeedbackFinishPage;
