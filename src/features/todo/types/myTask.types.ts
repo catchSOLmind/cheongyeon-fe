fc1df14ec2184d336349a12194a2d8504c0cc5de
Simmee02 <20211002@sungshin.ac.kr>
2026-02-10 10:59:56 +0900

    feat : 내 할일 추가하기 API 타입 변경 반영

--- src/features/todo/types/myTask.types.ts
+++ src/features/todo/types/myTask.types.ts
@@ -1,47 +1,29 @@
 // 내 할일 추가 
 // 내 할일 페이지에서 캘린더에 등록하기를 누르면 API 가 요청되는 형식
 // 요청 바디
-export type AddTasksRequest = {
-  date: string; // "YYYY-MM-DD"
-  taskTypeIds: number[];
+export type AddTasksResponse = {
+  createdCount: number;
+  created: AddedTask[];
 };
 
-// 추가된 task 정보
 export type AddedTask = {
   taskId: number;
   occurrenceId: number;
   taskTypeId: number;
   taskName: string;
   point: number;
-  time: string | null;            
-  assignee: AddedAssignee | null;  
-  repeat: AddedRepeat;            
+  time: string | null;
+  assignee: AddedAssignee | null;
+  repeat: AddedRepeat;
 };
 
-//할당
 export type AddedAssignee = {
   memberId: number;
   nickname: string;
   profileImageUrl: string | null;
 };
 
-//반복주기
 export type AddedRepeat = {
   enabled: boolean;
   daysOfWeek: string[];
 };
-
-
-// 200 OK 응답 바디
-export type AddTasksResponse = {
-  createdCount: number;
-  created: AddedTask[];
-};
-
-// 공통 에러 응답 바디
-export type ApiErrorResponse = {
-  isSuccess: boolean;
-  code: string;
-  message: string;
-  result: unknown; 
-}; 
